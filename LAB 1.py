48a596a890ce0938e6a1e928c3aefacdb862cd32
shreya-ai21 <139116160+shreya-ai21@users.noreply.github.com>
2023-07-20 14:37:01 +0530

    Update and rename list, tuple, dict.ipynb to LAB 1.py

---
+++ LAB 1.py
@@ -0,0 +1,12 @@
+import numpy as np
+x={'name':'Shreya',
+  'phone':8324456978,
+  'company':'Google'}
+print('name' in x)
+print('company' in x)
+x['age']=19
+print('age' in x)
+arr=[1,2,3,4,5,6]
+arr=np.array(arr,dtype=int)
+print(arr)
+arr.reshape(3,2)
